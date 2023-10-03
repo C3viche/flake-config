7560fb107fed2c9101930ff85def0d8bba9d9244
Cristian E <crisemble@gmail.com>
2023-10-03 00:24:05 -0700

    C3viche/old nixos (#2)
    * renamed folder to hold old configuration

---
+++ home-manager/zsh.nix
@@ -0,0 +1,22 @@
+{
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh.enable = true;
+    shellAliases = {
+      # Aliases to rebuild flake
+      nrs = "sudo nixos-rebuild switch --flake .#nixos";
+      hrs = "home-manager switch --flake .#c3viche@nixos";
+
+      # Collect garbage
+      ncg = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
+
+      # Nvim
+      vi = "nvim";
+      vim = "nvim";
+    };
+  };
+}
